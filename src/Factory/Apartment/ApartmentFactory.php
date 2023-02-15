afa3600c80d3ab9afa51e24e714436d3f5c26b1d
Ondřej Němec <ondrej.nemec@umimeweby.cz>
2023-02-15 10:20:21 +0100

    create ApartmentFactory

---
+++ src/Factory/Apartment/ApartmentFactory.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Factory\Apartment;
+
+use App\Entity\Apartments\Apartment;
+use App\Model\Apartment\ApartmentModel;
+
+class ApartmentFactory
+{
+
+    public function createBuilding(ApartmentModel $apartmentModel): Apartment
+    {
+
+    }
+}
