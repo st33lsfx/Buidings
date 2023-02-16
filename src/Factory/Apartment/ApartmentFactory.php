fc59188a28545b07dc2152fce7a87eae731bd609
Ondřej Němec <ondrej.nemec@umimeweby.cz>
2023-02-16 08:48:29 +0100

    fix ApartmentFactory

--- src/Factory/Apartment/ApartmentFactory.php
+++ src/Factory/Apartment/ApartmentFactory.php
@@ -3,6 +3,7 @@
 namespace App\Factory\Apartment;
 
 use App\Entity\Apartments\Apartment;
+use App\Entity\Building\Building;
 use App\Model\Apartment\ApartmentModel;
 
 class ApartmentFactory
@@ -10,6 +11,17 @@ class ApartmentFactory
 
     public function createBuilding(ApartmentModel $apartmentModel): Apartment
     {
+        $newApartment = new Apartment();
 
+        $newApartment->setTitle($apartmentModel->title);
+        $newApartment->setBuilding($apartmentModel->building);
+        $newApartment->setPerson($apartmentModel->person);
+        $newApartment->setSize($apartmentModel->size);
+        $newApartment->setColdWaterStatus($apartmentModel->coldWaterStatus);
+        $newApartment->setHotWaterStatus($apartmentModel->hotWaterStatus);
+        $newApartment->setGasMeterStatus($apartmentModel->gasMeterStatus);
+        $newApartment->setSquareStatus($apartmentModel->squareStatus);
+
+        return $newApartment;
     }
 }
