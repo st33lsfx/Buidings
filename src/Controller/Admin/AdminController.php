995ae9831ce1ce33ad34ab9f3b1ad8b0f3ae50cd
Ondřej Němec <ondrej.nemec@umimeweby.cz>
2023-02-21 14:21:23 +0100

    fix

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
-    #[Route('/admin', name: 'admin')]
+    #[Route('/', name: 'admin')]
     public function index(): Response
     {
         return $this->render('admin/index.html.twig', []);
